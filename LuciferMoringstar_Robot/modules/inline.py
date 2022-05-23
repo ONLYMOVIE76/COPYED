e6fb229906170e22a3a4d2e4573e97f6dda4da90
ONLYMOVIE76 <88230215+ONLYMOVIE76@users.noreply.github.com>
2022-05-23 16:43:10 +0530

    Update inline.py

--- LuciferMoringstar_Robot/modules/inline.py
+++ LuciferMoringstar_Robot/modules/inline.py
@@ -78,8 +78,8 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('📺Series', url='t.me/onlyseries76'),
+        InlineKeyboardButton('🎬 Movies', url='t.me/onlymovie76')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
